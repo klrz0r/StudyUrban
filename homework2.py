469644d97e8dc7a504f04b0fa2803c77ad0f0264
klrz0r <il95apkillerbroteam@yandex.ru>
2024-07-13 07:37:42 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+#Вторая домашняя работа
+#Задаем переменные
+quantityHomework = 12
+quantityHour = 1.5
+nameCourse = "Python"
+timeForOneTask = quantityHour/quantityHomework
+print(f'Курс: {nameCourse}, всего задач: {quantityHomework}, затрачено часов: {quantityHour}, среднее время выполнения {timeForOneTask} часа.')
