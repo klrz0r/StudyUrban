7c372083ce34332c6cc01613163eff9e738606fe
klrz0r <il95apkillerbroteam@yandex.ru>
2024-07-14 18:46:16 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+#4th homework.
+my_string = input("Enter string: ")
+print(len(my_string))
+print("Строка в верхнем регистре:", my_string.upper())
+print("Строка в ниженем регистре:", my_string.lower())
+print("Строка без пробелов:", my_string.replace(" ",""))
+print("Первый символ строки:", my_string[0])
+print("Последний ссимвол строки: ", my_string[-1])
